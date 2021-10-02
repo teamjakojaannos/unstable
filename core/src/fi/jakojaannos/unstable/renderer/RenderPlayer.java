7cac9b382c254346e8f9d3cfab8a8e965a1b27d2
toilari <ilari97@hotmail.com>
2021-10-02 18:51:18 +0300

    player anim

--- core/src/fi/jakojaannos/unstable/renderer/RenderPlayer.java
+++ core/src/fi/jakojaannos/unstable/renderer/RenderPlayer.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
+import fi.jakojaannos.unstable.UnstableGame;
 import fi.jakojaannos.unstable.components.PhysicsBody;
 import fi.jakojaannos.unstable.components.PlayerHudComponent;
 import fi.jakojaannos.unstable.ecs.EcsSystem;
@@ -15,6 +16,9 @@ import fi.jakojaannos.unstable.resources.Resources;
 public class RenderPlayer implements EcsSystem<RenderPlayer.Input>, AutoCloseable {
     private final SpriteBatch spriteBatch;
     private final Texture texture;
+    private final Texture textureIdle;
+    private final TextureRegion[] frames;
+    private final TextureRegion[] idleFrames;
     private final Texture closetIndicator;
 
     private final Sound[] sounds;
@@ -22,6 +26,19 @@ public class RenderPlayer implements EcsSystem<RenderPlayer.Input>, AutoCloseabl
     public RenderPlayer(SpriteBatch spriteBatch) {
         this.spriteBatch = spriteBatch;
         this.texture = new Texture("Journalist.png");
+        this.textureIdle = new Texture("Journalist_idle.png");
+        this.frames = new TextureRegion[8];
+
+        final var frameWidth = this.texture.getWidth() / this.frames.length;
+        final var frameHeight = this.texture.getHeight();
+        for (int frameIndex = 0; frameIndex < this.frames.length; frameIndex++) {
+            this.frames[frameIndex] = new TextureRegion(this.texture, frameWidth * frameIndex, 0, frameWidth, frameHeight);
+        }
+
+        this.idleFrames = new TextureRegion[]{
+                new TextureRegion(this.textureIdle)
+        };
+
         this.closetIndicator = new Texture("badlogic.jpg");
 
         this.sounds = new Sound[]{
@@ -48,9 +65,17 @@ public class RenderPlayer implements EcsSystem<RenderPlayer.Input>, AutoCloseabl
                  final var originX = 0.0f;
                  final var originY = 0.0f;
 
-                 final var region = physics.facingRight
-                         ? new TextureRegion(this.texture, 32, 48)
-                         : new TextureRegion(this.texture, 32, 0, -32, 48);
+                 final var tick = resources.timeManager.currentTick();
+                 final var framesToUse = physics.speed > 0.001f
+                         ? this.frames
+                         : this.idleFrames;
+
+                 final var loopDuration = 1.25;
+
+                 final var scaledTick = ((float) tick / (float) UnstableGame.Constants.GameLoop.TICKS_PER_SECOND) / (loopDuration / framesToUse.length);
+                 final var region = framesToUse[((int) scaledTick) % framesToUse.length];
+                 region.flip(region.isFlipX() == physics.facingRight, false);
+
                  this.spriteBatch.draw(region, x, y, originX, originY, width, height, 1.0f, 1.0f, 0.0f);
 
                  if (entity.hud.currentIndicator == PlayerHudComponent.Indicator.CLOSET) {
@@ -64,7 +89,9 @@ public class RenderPlayer implements EcsSystem<RenderPlayer.Input>, AutoCloseabl
                  }
                  this.spriteBatch.end();
 
-                 if (!resources.timers.isActiveAndValid(physics.footstepTimer) && physics.speed > 0.01f) {
+                 if (physics.speed == 0f) {
+                     resources.timers.clear(physics.footstepTimer);
+                 } else if (!resources.timers.isActiveAndValid(physics.footstepTimer)) {
                      final var footstepTime = 0.5f;
                      physics.footstepTimer = resources.timers.set(footstepTime, false, () -> {
                          final var sound = this.sounds[MathUtils.random(this.sounds.length - 1)];
@@ -77,6 +104,7 @@ public class RenderPlayer implements EcsSystem<RenderPlayer.Input>, AutoCloseabl
     @Override
     public void close() {
         this.texture.dispose();
+        this.textureIdle.dispose();
         for (final var sound : this.sounds) {
             sound.dispose();
         }
